5b68b8063046983baf07c87bf18ec517de50dad9
Makhkambek <hakimjanovmahkam@gmail.com>
2026-03-16 13:09:48 +0500

    Done hero class

--- src/com/narxoz/rpg/hero/HeroProfile.java
+++ src/com/narxoz/rpg/hero/HeroProfile.java
@@ -18,13 +18,10 @@ public class HeroProfile {
     }
 
     public void takeDamage(int amount) {
-        health -= amount;
-        if (health < 0) {
-            health = 0;
-        }
+         this.health = Math.max(0, this.health - amount);
     }
 
     public boolean isAlive() {
-        return health > 0;
+       return this.health > 0;
     }
 }
